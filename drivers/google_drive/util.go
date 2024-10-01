29163c452172b3f99bddc840e4d9db23fbb508a5
KaySar12 <shintheman12@gmail.com>
2024-10-01 14:40:32 +0700

    remove secret

--- drivers/google_drive/util.go
+++ drivers/google_drive/util.go
@@ -17,8 +17,8 @@ import (
 )
 
 var (
-	client_id     = "176137744326-qrfhqueo0dro0tmoh7f96o2vphtmlor8.apps.googleusercontent.com"
-	client_secret = "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"
+	client_id     = ""
+	client_secret = ""
 )
 
 // https://api.nextzenos.com/cloud
